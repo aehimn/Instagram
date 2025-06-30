43589ac90775781b5e0136dea249cd1d516cb2be
김해민 <heaminkim1007@gmail.com>
2025-06-30 18:55:00 +0900

    [김해민] - timeline view 구현

--- src/main/java/com/haemin/Instagram/post/PostController.java
+++ src/main/java/com/haemin/Instagram/post/PostController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PostController {
 	
 	@GetMapping("/timeline-view")
-	public String postTimeline() {
+	public String timeline() {
 		return "post/timeline";
 	}
 
--- src/main/java/com/haemin/Instagram/user/UserController.java
+++ src/main/java/com/haemin/Instagram/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 // View를 위한 Controller
 @RequestMapping("/user")
 @Controller
@@ -17,6 +19,13 @@ public class UserController {
 	@GetMapping("/login-view")
 	public String loginInput() {
 		return "user/login";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		return "redirect:/user/login-view";
 	}
 
 }
